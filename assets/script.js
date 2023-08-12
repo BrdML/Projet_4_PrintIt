90f7d9bbfa0db18cf852eda67513707a0de709ac
BrdML <bert.m310@gmail.com>
2023-08-12 15:26:45 +0200

    Ajout des bullets points

--- assets/script.js
+++ assets/script.js
@@ -22,6 +22,7 @@ const arrowRight = document.querySelector('.arrow_right');
 const arrowLeft = document.querySelector('.arrow_left');
 const dots = document.querySelector('.dots');
 
+// Flèches slider
 arrowRight.addEventListener("click", ()=>{
 	console.log('Flèche droite')
 });
@@ -29,3 +30,19 @@ arrowRight.addEventListener("click", ()=>{
 arrowLeft.addEventListener("click", ()=>{
 	console.log('Flèche gauche')
 });
+
+// Ajout des bullets points du slider
+const compteurDots = function() {
+	//boucle sur le tableau slides pour créer le nombre exact de bullets points
+	for (let i = 0; i < slides.length; i++) {
+	// création d'un élément div
+	const dot = document.createElement("div") 
+	// applique les propriéters CSS au "dot"
+	dot.className = "dot"
+	// ajoute les éléments "dot" dans le parent dots
+	dots.appendChild(dot)
+	}
+}
+
+// Appel de la fonction
+compteurDots();
